15f207dbcaf96c56230412949bb7b42efc43b7c6
Sergei Savelev <sergcen17@gmail.com>
2016-04-16 09:54:36 +0300

    test

--- app/scripts/comments/comments-collection.js
+++
@@ -1,7 +0,0 @@
-import {Collection} from 'backbone';
-import Model from './comments-model';
-
-export default Collection.extend({
-  url: '/api/comments.json',
-  model: Model
-});
--- app/scripts/comments/comments-layout.js
+++
@@ -1,7 +0,0 @@
-import { CollectionView } from 'backbone.marionette';
-import CommentView from './comments-view'
-
-export default CollectionView.extend({
-  className: 'comments-container',
-  childView: CommentView
-});
