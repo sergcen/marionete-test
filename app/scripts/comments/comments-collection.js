145ccb62f6d62260ccdfa433fb70291e7820532d
Sergei Savelev (Akvelon) <v-sesave@microsoft.com>
2016-04-26 10:46:48 -0700

    added muiltiple source loading to collection

--- app/scripts/comments/comments-collection.js
+++ app/scripts/comments/comments-collection.js
@@ -1,7 +1,19 @@
+import $ from 'jquery';
 import {Collection} from 'backbone';
 import Model from './comments-model';
 
 export default Collection.extend({
-  url: '/api/comments.json',
-  model: Model
+  url: ['/api/comments.json','/api/product_comments.json'],
+  model: Model,
+  fetch() {
+    return $.when.apply($,
+        this.url.map((url) => $.ajax(url).done(d => {
+          this.add(d.map(item => {
+            // set unique id for each type
+            item.id = item.id + '_' + item.type;
+            return item;
+          }))
+        }))
+      )
+  }
 });
